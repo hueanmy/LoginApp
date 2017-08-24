8178a875e6b94c1b3b868a9820a2a412fa039423
lecongchinh <lecongchinh.ptit@gmail.com>
2017-08-24 10:51:34 +0700

    add login with local part 2

--- app/http/router/router.js
+++ app/http/router/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const registerController = require('../controller/register-controller');
 let router = express.Router();
 
-router.get('/register', registerController.get);
+router.get('/register', registerController.getRegister);
+router.post('/register', registerController.postRegister);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 const passport = require('passport');
 const nunjucks = require('nunjucks');
 const router = require('./app/http/router/router');
+const path = require('path');
 
 let app = express();
 
@@ -19,6 +21,24 @@ nunjucks.configure('./views', {
     autoescape: true,
     express: app
 } ) ;
+
+//express-validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+            , root    = namespace.shift()
+            , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
+    }
+}));
 
 app.use(router);
 
