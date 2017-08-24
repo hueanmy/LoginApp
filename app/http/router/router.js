e686e75d410900394dfb63a5a559ceba6d1abd1c
hueanmy <ltmai93@gmail.com>
2017-08-24 15:07:05 +0700

    add layout

--- app/http/router/router.js
+++ app/http/router/router.js
@@ -2,6 +2,6 @@ const express = require('express');
 const registerController = require('../controller/register-controller');
 let router = express.Router();
 
-router.get('/register', registerController.get);
+router.get('/register', registerController.getRegister);
 
 module.exports = router;
